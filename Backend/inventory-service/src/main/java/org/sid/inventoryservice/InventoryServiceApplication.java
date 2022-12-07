2bb071e63a4d87f25092323e8231c0db3053a6a0
NassimMs <49593722+NassimMs@users.noreply.github.com>
2022-12-07 20:04:48 +0000

    Added Backend Microservices
    Inventory , Customer , Billing , The Gateway and Eureka Server

---
+++ Backend/inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,31 @@
+package org.sid.inventoryservice;
+
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository, RepositoryRestConfiguration restConfiguration){
+        restConfiguration.exposeIdsFor(Product.class);
+        return args -> {
+            productRepository.save(new Product(null,"Ordinateur",788,12));
+            productRepository.save(new Product(null,"Imprimante",788,12));
+            productRepository.save(new Product(null,"Smartphone",788,12));
+            productRepository.findAll().forEach(p->{
+                System.out.println(p.toString());
+            });
+        };
+    }
+
+}
